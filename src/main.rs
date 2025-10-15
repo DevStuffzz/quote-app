ef05fcfea9d2193e5180b4e763b48d57bb4875da
Corey Beaver <coreybeaver@Coreys-MacBook-Pro.local>
2025-10-15 04:49:49 -0400

    changed binding of http server

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(Cors::permissive()) // allow CORS for development
             .service(random_quote)
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:8080")?
     .run()
     .await
 }
